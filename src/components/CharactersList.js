0b87598bf25849e33a32cd37d0f3b2f278f38393
Huveteau Tristan <tristan.huveteau@etu.univ-poitiers.fr>
2023-12-04 18:17:21 +0100

    eval_lundi

--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -1,15 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { format } from 'date-fns';
 
 export function CharactersList({ characters = [] }) {
-    return (
-        <ul id="characters">
-            {characters.map((character) => (
-                <li key={character.id}>
-                    <Link to={`/characters/${character.id}`}>
-                        {character.name}
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    );
+  // Fonction pour formater la date avec date-fns
+  const formatModifiedDate = (isoDate) => {
+    const date = new Date(isoDate);
+    return format(date, 'MMMM dd, yyyy'); // Format de date souhaité
+  };
+
+  return (
+    <ul id="characters">
+      {characters.map((character) => (
+        <li key={character.id}>
+          <Link to={`/characters/${character.id}`}>
+            <strong>{character.name} - </strong>
+            {formatModifiedDate(character.modified)}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
--- src/components/NumberOfCharacters.test.js
+++ src/components/NumberOfCharacters.test.js
@@ -1,26 +1,8 @@
-import { render } from '@testing-library/react';
-import { NumberOfCharacters } from './NumberOfCharacters';
-
-describe('NumberOfCharacters', () => {
-
-  it('renders "There is no characters" when characters prop is empty', () => {
-    const { getByText } = render(<NumberOfCharacters characters={[]} />);
-    expect(getByText('There is no characters')).toBeInTheDocument();
-  });
-
-  it('renders "There is no characters" when characters prop is not passed', () => {
-    const { getByText } = render(<NumberOfCharacters />);
-    expect(getByText('There is no characters')).toBeInTheDocument();
-  });
-
-  it('renders "There is 1 character" when characters prop has 1 item', () => {
-    const { getByText } = render(<NumberOfCharacters characters={['Iron Man']} />);
-    expect(getByText('There is 1 character')).toBeInTheDocument();
-  });
-
-  it('renders "There is 2 characters" when characters prop has 2 items', () => {
-    const { getByText } = render(<NumberOfCharacters characters={['Iron Man', 'Spider-Man']} />);
-    expect(getByText('There is 2 characters')).toBeInTheDocument();
-  });
-
-});
+export function NumberOfCharacters({ characters = [] }) {
+    return (
+      <p>
+        There is {characters.length === 0 ? 'no' : characters.length} character{characters.length !== 1 ? 's' : ''}
+      </p>
+    );
+  }
+  
